fc4e5506bebd7f66139e81415d89dce962ea3c4a
Soulaimane Bouabid <sbouabid@e2r12p4.1337.ma>
2024-02-18 10:05:18 +0100

    shell

--- list.c
+++ list.c
@@ -6,7 +6,7 @@
 /*   By: sbouabid <sbouabid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 10:30:17 by sbouabid          #+#    #+#             */
-/*   Updated: 2024/02/17 12:52:52 by sbouabid         ###   ########.fr       */
+/*   Updated: 2024/02/18 10:00:07 by sbouabid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,20 @@ void	ft_lstadd_back(t_node **lst, t_node *new)
 t_node	*ft_lstnew(char *ptr)
 {
 	t_node	*node;
+	char	*path;
 
 	char **str = ft_split(ptr, ' ');
 	node = malloc(sizeof(t_node));
 	if (!node)
 		return (NULL);
-	node->command = strdup(getCommandPath(str[0]));
+	path = getCommandPath(str[0]);
+	if (path == NULL)
+	{
+		printf("command not found: %s\n", str[0]);
+		free(node);
+		return NULL;
+	}
+	node->command = strdup(path);
 	node->arg = str;
 	node->next = NULL;
 	return (node);
